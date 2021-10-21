6c8b322187491411ddadb02675ce2145d31b39fd
Ben Duncan <86330277+DuncanDonutz@users.noreply.github.com>
2021-10-20 21:02:31 -0500

    setting up splashscreen
    deleted previous raw files, edited comments

--- app/src/main/java/com/duncan/repea/ui/view/SplashScreenActivity.kt
+++ app/src/main/java/com/duncan/repea/ui/view/SplashScreenActivity.kt
@@ -12,32 +12,16 @@ import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     companion object {
-
-        //NOTE: It is recommended to keep the time less than 3000 ms
-        // but as this is just going to be demo so I'm using 6000 ms
-        // to show the full animation.
         const val ANIMATION_TIME: Long = 6000 //Change time according to your animation.
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-        //Use Handler to delay the start of next Activity
-        // and show animation in the mean while
+        // Handler to delay the start of next Activity
         Handler(this.mainLooper).postDelayed({
-
-            //This block will be executed after ANIMATION_TIME milliseconds.
-
-            //After ANIMATION_TIME we will start the MainActivity
             startActivity(Intent(this, MainActivity::class.java))
-
-            //To remove this activity from back stack so that
-            // this activity will not show when user closes MainActivity
             finish()
-
         }, ANIMATION_TIME)
-
     }
-
 }
