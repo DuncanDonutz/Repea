605ce5bd7143164653ef84d13d62467ee4cf5c57
Ben Duncan <86330277+DuncanDonutz@users.noreply.github.com>
2021-10-22 15:47:28 -0500

    Finished splash screen
    Added JSON animation, image assets. Updated AndroidManifest, SplashScreenActivity and its XML

--- app/src/main/java/com/duncan/repea/ui/view/SplashScreenActivity.kt
+++ app/src/main/java/com/duncan/repea/ui/view/SplashScreenActivity.kt
@@ -12,13 +12,18 @@ import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     companion object {
-        const val ANIMATION_TIME: Long = 6000 //Change time according to your animation.
+        const val ANIMATION_TIME: Long = 1200
     }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        // Handler to delay the start of next Activity
+
+        splash_screen.playAnimation()
+
+        // Delay start of MainActivity
         Handler(this.mainLooper).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
